0034be2fd777f09cb1002959c275c9e490960abc
dinaahmed4 <100288547+dinaahmed4@users.noreply.github.com>
2023-01-24 17:53:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+//component app
+import './App.css';
+import Navbar from './Navbar';
+import Media from './Media';
+import Details from './Details';
+import Device from './Device';
+import Web from './Web';
+import Stop from './Stop';
+import Footer from './Footer';
+//css style
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Media />
+      <Details />
+      <Device />
+      <Web />
+      <Stop />
+      <Footer />
+
+
+
+    </div>
+  );
+}
+
+export default App;
